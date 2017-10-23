acd82a85b8296c06fd1efcfdfaa9676892504f25
Agustina Riquelme <agustinariq@gmail.com>
2017-10-22 22:56:50 -0300

    El enemigo sigue al jugador al caminar y al atacar

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	public Rigidbody2D rigidBody;
-	public PolygonCollider2D collider;
+    public Rigidbody2D rigidBody;
+    public PolygonCollider2D collider;
+    public GameObject player;
+    public GameObject enemy;
+    public Animator playerAnimator;
+    public SpriteRenderer enemySpriteRenderer;
 
-	// Use this for initialization
-	void Start () {
-		rigidBody = GetComponent<Rigidbody2D> ();
-		collider = GetComponent<PolygonCollider2D> ();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		rigidBody.velocity = new Vector2 (-2, 0);
-	}
+    // Use this for initialization
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<PolygonCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerAnimator = player.GetComponent<Animator>();
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemySpriteRenderer = enemy.GetComponent<SpriteRenderer>();
+    }
 
-	void OnCollisionEnter2D(Collision2D collision){
-		if (collision.gameObject.tag == "Limit"){
-			rigidBody.velocity = new Vector2(10,0);
-		}
+    // Update is called once per frame
+    void FixedUpdate() {
+        followPlayer();
+        lookTowardPlayer();
+    }
 
-	}
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("saranga");
+        rigidBody.velocity = new Vector2(0, 0);
+    }
+
+    // enemy follows player when he walks
+    void followPlayer() {
+        Vector2 player2Position = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 offset = new Vector2(0.75f, 0);
+        if (playerAnimator.GetBool("walking") || playerAnimator.GetBool("isAttacking")) {
+            // current, target (offset para que no se pisen los prites), velocity
+            transform.position = Vector2.MoveTowards(transform.position, (player2Position + offset), 0.024f);
+        }
+    }
+    // enemy always looks at the direction of the player
+    void lookTowardPlayer() {
+        if (enemy.transform.position.x < player.transform.position.x) {
+            enemySpriteRenderer.flipX = true;
+        }
+        else {
+            enemySpriteRenderer.flipX = false;
+        }
+    }
 }
