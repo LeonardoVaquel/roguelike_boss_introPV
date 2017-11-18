5677d713f940a4966acf3a4b0ab02c3f473837f3
LeonardoVaquel <leonardovaquel@gmail.com>
2017-11-18 13:43:09 -0300

    Retoque de GUI y organizacion de objetos dentro de la escna

--- Assets/scripts/HealthPotionItem.cs
+++ Assets/scripts/HealthPotionItem.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthPotionItem : AbstractItem {
+	public PlayerStats player = GameObject.FindObjectOfType<PlayerStats> ();
 
 	public HealthPotionItem(): base(true, "Esta posion te cura 5 puntos de vida."){
 		
 	}
 
 	public void UseItem ()	{
-		player.SendMessage ("heal", 5);
+		player.hp = player.hp + 5;
 	}
 }
