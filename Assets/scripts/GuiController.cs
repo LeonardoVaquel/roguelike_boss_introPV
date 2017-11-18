2f919a704e783520327b31ca3760c5ea2e2b471f
LeonardoVaquel <leonardovaquel@gmail.com>
2017-11-18 11:30:17 -0300

    Se agrego GUI de stats y bindeo de los valores:
    * Se abre con la tecla 'C'

---
+++ Assets/scripts/GuiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiController : MonoBehaviour {
+
+	public Canvas statsCanvas;
+	private bool enableCanvas;
+	public PlayerStats statsScript;
+
+	// Use this for initialization
+	void Start () {
+		statsCanvas = GameObject.Find ("StatsCanvas").GetComponent<Canvas> ();
+		statsScript = GameObject.Find ("PlayerStats").GetComponent<PlayerStats> ();
+		enableCanvas = false;
+		statsCanvas.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (enableCanvas) {
+				statsCanvas.gameObject.SetActive (false);
+				enableCanvas = false;
+			} else {
+				
+				statsCanvas.gameObject.SetActive (true);
+				statsScript.refreshStats ();
+				enableCanvas = true;
+			}
+		} 
+
+	}
+}
--- Assets/scripts/PlayerStats.cs
+++ Assets/scripts/PlayerStats.cs
@@ -1,20 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour {
 
-        public int lvl = 1;
-        public int exp = 0;
-        public int hp = 100;
-        public int maxHP = 100;
-        public int mp = 30;
-        public int str = 10;
-        public int res = 8;
+        public int lvl;
+        public int exp;
+        public int hp;
+        public int maxHP;
+        public int mp;
+        public int str;
+        public int res;
         // magic power
-        public int mnd = 1;
+        public int mnd;
         //magic defense
-        public int spr = 1;
-        public int dex = 5;
+        public int spr;
+        public int dex;
 
-    }
+	public Text lvlValue;
+	public Text expValue;
+	public Text mpValue;
+	public Text strValue;
+	public Text resValue;
+	public Text mndValue;
+	public Text dexValue;
+
+	public Canvas statsCanvas;
+
+	void Start(){
+		lvlValue 	= GameObject.Find ("LvlValue").GetComponent<Text> ();
+		expValue 	= GameObject.Find ("ExpValue").GetComponent<Text> ();
+		mpValue		= GameObject.Find ("EnergiaValue").GetComponent<Text> ();
+		strValue	= GameObject.Find ("StrValue").GetComponent<Text> ();
+		resValue	= GameObject.Find ("ResValue").GetComponent<Text> ();
+		mndValue	= GameObject.Find ("MagicDamageValue").GetComponent<Text> ();
+		dexValue	= GameObject.Find ("DexValue").GetComponent<Text> ();
+		statsCanvas = GameObject.Find ("StatsCanvas").GetComponent<Canvas> ();
+
+		lvl = 1;
+		exp = 0;
+		str = 10;
+		hp = 100;
+		maxHP = 100;
+		mp = 30;
+		res = 8;
+		mnd = 1;
+		dex = 5;
+	}
+
+	void FixedUpdate(){
+
+		refreshStats ();
+	}
+
+	public void refreshStats(){
+		lvlValue.text 	= lvl.ToString ();
+		expValue.text	= exp.ToString ();
+		mpValue.text 	= mp.ToString ();
+		strValue.text	= str.ToString ();
+		resValue.text	= res.ToString ();
+		mndValue.text	= mnd.ToString ();
+		dexValue.text	= dex.ToString ();
+	}
+}
